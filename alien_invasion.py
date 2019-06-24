ae04d63217d3edc84b1f3f4a0c50d0a5285b2c03
Your Name <maazahmed2k16@gmail.com>
2019-06-24 12:53:01 -0700

    Display game window

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,22 +2,17 @@ import pygame
 import sys
 
 
-
-
-
 # def run_game():
 #     pygame.init()
 #     pygame.display.set_mode((600, 600))
 #     pygame.display.set_caption("Alien Invasion")
 #     while True:
-#         for event in pygame.event.get():        
-#             if event.type == pygame.QUIT:  
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
 #                 sys.exit()
 #             pygame.display.flip()
 
 # run_game()r
-
-
 
 
 # >>>>>>>>>>>>>>>>>>>> Creating Game Window
@@ -34,11 +29,14 @@ import sys
 
 
 def run_game():
-        pygame.init()
-        screen =   pygame.display.set_mode((800, 600))
-        
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("git push -u origin master")
+    screen.fill((230, 230, 230))
+    while True:
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
 
-
-
-
-run_game()        
+run_game()
