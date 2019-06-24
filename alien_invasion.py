9472ad58a8526fe66bb6f68dbc2db374547e8cad
Your Name <maazahmed2k16@gmail.com>
2019-06-24 12:44:19 -0700

    Updated

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,13 +18,27 @@ import sys
 # run_game()r
 
 
+
+
+# >>>>>>>>>>>>>>>>>>>> Creating Game Window
+# def run_game():
+#     pygame.init()
+#     screen = pygame.display.set_mode((900, 600))
+#     pygame.display.set_caption("Alien Invasion")
+#     while True:
+#         for event in pygame.event.get():
+#             if event == pygame.QUIT:
+#                 sys.exit()
+#         pygame.display.flip()
+# run_game()
+
+
 def run_game():
-    pygame.init()
-    screen = pygame.display.set_mode((900, 600))
-    pygame.display.set_caption("Alien Invasion")
-    while True:
-        for event in pygame.event.get():
-            if event == pygame.QUIT:
-                sys.exit()
-        pygame.display.flip()
-run_game()
+        pygame.init()
+        screen =   pygame.display.set_mode((800, 600))
+        
+
+
+
+
+run_game()        
