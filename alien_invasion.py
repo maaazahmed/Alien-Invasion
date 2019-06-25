3e1e962d223ba9e312fd11a3d695784562e696c5
Your Name <maazahmed2k16@gmail.com>
2019-06-25 02:19:47 -0700

    updated

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,42 +1,34 @@
-import pygame
-import sys
-
+# import pygame
+# import sys
 
 # def run_game():
 #     pygame.init()
-#     pygame.display.set_mode((600, 600))
-#     pygame.display.set_caption("Alien Invasion")
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 sys.exit()
-#             pygame.display.flip()
-
-# run_game()r
-
-
-# >>>>>>>>>>>>>>>>>>>> Creating Game Window
-# def run_game():
-#     pygame.init()
-#     screen = pygame.display.set_mode((900, 600))
-#     pygame.display.set_caption("Alien Invasion")
+#     screen = pygame.display.set_mode((800, 600))
+#     pygame.display.set_caption("git push -u origin master")
+#     screen.fill((230, 230, 230))
 #     while True:
 #         for event in pygame.event.get():
 #             if event == pygame.QUIT:
 #                 sys.exit()
 #         pygame.display.flip()
+
 # run_game()
+
+
+import pygame
+import sys
 
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("git push -u origin master")
+    screen = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption("Alien Invasion")
     screen.fill((230, 230, 230))
     while True:
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 sys.exit()
         pygame.display.flip()
+
 
 run_game()
